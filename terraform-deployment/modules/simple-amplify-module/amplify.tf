35365aa37cf9b541f8909e96b4eefa3dcf524fa3
zoltan-fodi <zoltan-fodi@pluralsight.com>
2023-09-29 23:17:59 +0200

    npm i instead of ci (package-lock is not pushed to the repo)

--- terraform-deployment/modules/simple-amplify-module/amplify.tf
+++ terraform-deployment/modules/simple-amplify-module/amplify.tf
@@ -32,7 +32,7 @@ resource "aws_amplify_app" "app" {
   }
 
   environment_variables = {
-    REGION = "${data.aws_region.current.id}"
+    REGION         = "${data.aws_region.current.id}"
     RESEND_API_KEY = var.RESEND_API_KEY
   }
 }
